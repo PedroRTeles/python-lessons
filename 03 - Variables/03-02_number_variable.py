6e1ebfc66b73410bec390ac3778fb1342f72d104
Pedro Teles <pedroateles@protonmail.com>
2020-09-03 20:29:48 -0300

    added number variable lesson

---
+++ 03 - Variables/03-02_number_variable.py
@@ -0,0 +1,5 @@
+number1 = int(input('Type the first number: '))
+number2 = int(input('Type the second number: '))
+sum = number1 + number2
+message = 'The sum of {} and {} is {}'.format(number1, number2, sum)
+print(message)
